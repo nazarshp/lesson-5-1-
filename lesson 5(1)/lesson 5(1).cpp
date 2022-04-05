da7c19c496837bbc757a0044acd1be03f0ef96b9
Nazar Shpak <nazar.shpak2004@gmail.com>
2022-04-05 23:16:28 +0300

    Add project files.

---
+++ lesson 5(1)/lesson 5(1).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int calculate(int a,int b,char c)
+{
+    switch (c)
+    {
+    case '+':
+            return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
+    case '%':
+        return a % b;
+    default:
+        std::cout << "Erorr";
+        break;
+    }
+}
+
+
+
+int main()
+{
+    int a;
+    int b;
+    char c;
+    std::cout << "Enter 1 number: ";
+    std::cin >> a;
+    std::cout << "Enter 2 number: ";
+    std::cin >> b;
+    std::cout << "Enter symbol +-*/%: ";
+    std::cin >> c;
+
+    std::cout << calculate(a, b, c);
+}
